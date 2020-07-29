bbcd1dad4d62db30be6cc9570702dd31efab6b2f
richard929 <richard929@gmail.com>
2020-07-29 16:25:54 -0400

    update linkedin profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="site-social">
         <div className="site-social__list">
           <a
-            href="https://www.linkedin.com/in/richard-zhan-0a3a211a0/"
+            href="https://www.linkedin.com/in/richard-zhan-a936a11b0/"
             className="site-social__link"
           >
             <svg
